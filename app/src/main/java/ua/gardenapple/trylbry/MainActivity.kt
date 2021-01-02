4a5886773effe2f7ca68cac83b305eecb0aa0915
gardenapple <gardenapple@posteo.net>
2021-01-02 20:15:13 +0200

    Proper dialog UI

--- app/src/main/java/ua/gardenapple/trylbry/MainActivity.kt
+++ app/src/main/java/ua/gardenapple/trylbry/MainActivity.kt
@@ -1,16 +1,23 @@
 package ua.gardenapple.trylbry
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.ServiceConnection
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ShareCompat
+import androidx.fragment.app.DialogFragment
+import kotlinx.coroutines.*
 import org.json.JSONObject
 import org.json.JSONTokener
+import ua.gardenapple.trylbry.databinding.DialogMainBinding
 import java.io.BufferedInputStream
 import java.net.URL
 import java.net.URLEncoder
@@ -18,79 +25,126 @@ import java.nio.charset.StandardCharsets
 import java.util.concurrent.Executor
 import javax.net.ssl.HttpsURLConnection
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     companion object {
         private const val LBRY_COM_API = "https://api.lbry.com"
+        private const val OPEN_LBRY_COM = "https://open.lbry.com"
 
         private val videoIdPattern = Regex("""[/?]v[=/]([^&/?]*)""")
         private val videoIdPatternShort = Regex("""https?://youtu\.be/([^?/]*)""")
         private val channelIdPattern = Regex("""/channel/([^?/]*)""")
     }
+    
+    private enum class ContentType {
+        VIDEO, CHANNEL
+    }
+    
+    private lateinit var lbryUri: Uri
+    private lateinit var binding: DialogMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        
+        binding = DialogMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         
         val youtubeUrl = intent!!.data.toString()
-        var id = ""
-        var isVideo = false
-        var regexSuccess = false
-
-        videoIdPattern.find(youtubeUrl)?.run {
-            id = this.groupValues[1]
-            isVideo = true
-            regexSuccess = true
-        }
-        if (!regexSuccess) {
-            videoIdPatternShort.find(youtubeUrl)?.run {
-                id = this.groupValues[1]
-                isVideo = true
-                regexSuccess = true
-            }
-        }
-        if (!regexSuccess) {
-            channelIdPattern.find(youtubeUrl)?.run {
-                id = this.groupValues[1]
-                isVideo = false
-                regexSuccess = true
-            }
-        }
-        if (!regexSuccess) {
-            throw IllegalArgumentException("Could not get content ID from YouTube URL")
-        }
-
+        val id: String
+        val contentType: ContentType
         
-        runBlocking(Dispatchers.IO) {
-            val lbryChannel = resolveYoutube(id, isVideo)
-            val lbryUri = Uri.parse("https://open.lbry.com/${lbryChannel.replace('#', ':')}")
-            val intent = Intent(Intent.ACTION_VIEW, lbryUri)
-            startActivity(intent)
+        when {
+            videoIdPattern.containsMatchIn(youtubeUrl) -> {
+                id = videoIdPattern.find(youtubeUrl)!!.groupValues[1]
+                contentType = ContentType.VIDEO
+            }
+            videoIdPatternShort.containsMatchIn(youtubeUrl) -> {
+                id = videoIdPatternShort.find(youtubeUrl)!!.groupValues[1]
+                contentType = ContentType.VIDEO
+            }
+            channelIdPattern.containsMatchIn(youtubeUrl) -> {
+                id = channelIdPattern.find(youtubeUrl)!!.groupValues[1]
+                contentType = ContentType.CHANNEL
+            }
+            else -> throw IllegalArgumentException("Could not get content ID from YouTube URL")
+        }
+
+        binding.message.text = resources.getString(
+                when(contentType) {
+                    ContentType.VIDEO -> R.string.dialog_checking_youtube_video
+                    ContentType.CHANNEL -> R.string.dialog_checking_youtube_channel
+                }
+        )
+        binding.watchOnLbry.setOnClickListener {
+            val viewIntent = Intent(Intent.ACTION_VIEW, lbryUri)
+            startActivity(viewIntent)
             finish()
+        }
+        binding.watchOnYoutube.setOnClickListener {
+            startYoutubeActivity()
+            finish()
+        }
+        binding.cancelButton.setOnClickListener {
+            finish()
+        }
+        
+        launch {
+            val lbryChannel = resolveYoutube(id, contentType)
+            if (lbryChannel == null) {
+                startYoutubeActivity()
+                finish()
+            } else {
+                lbryUri = Uri.parse("$OPEN_LBRY_COM/${lbryChannel.replace('#', ':')}")
+                binding.watchOnLbry.visibility = View.VISIBLE
+                binding.message.text = resources.getString(when (contentType) {
+                    ContentType.VIDEO -> R.string.dialog_found_youtube_video
+                    ContentType.CHANNEL -> R.string.dialog_found_youtube_channel
+                })
+                binding.progressBar.visibility = View.GONE
+            }
         }
     }
 
-    private suspend fun resolveYoutube(id: String, isVideo: Boolean = true): String {
-        val queryParameter = if (isVideo)
-            "video_ids"
-        else
-            "channel_ids"
+    override fun onDestroy() {
+        super.onDestroy()
+
+        cancel()
+    }
+
+    private suspend fun resolveYoutube(id: String, contentType: ContentType): String? {
+        val queryParameter = when(contentType) {
+            ContentType.VIDEO -> "video_ids"
+            ContentType.CHANNEL -> "channel_ids"
+        }
 
         val apiUrl = URL("$LBRY_COM_API/yt/resolve?$queryParameter=${URLEncoder.encode(id, "UTF-8")}")
 
-        return withContext(Dispatchers.IO) {
+        val responseString = withContext(Dispatchers.IO) {
             val connection = apiUrl.openConnection() as HttpsURLConnection
-            val responseString = connection.inputStream.reader().use {
+            connection.inputStream.reader().use {
                 it.readText()
             }
-
-            val responseJson = JSONObject(responseString)
-            if (!responseJson.getBoolean("success")) {
-                throw RuntimeException(responseJson.getString("error"))
-            }
-
-            responseJson.getJSONObject("data")
-                .getJSONObject(if (isVideo) "videos" else "channels")
-                .getString(id)
         }
+
+        val responseJson = JSONObject(responseString)
+        if (!responseJson.getBoolean("success")) {
+            throw RuntimeException(responseJson.getString("error"))
+        }
+
+        val resultJson = responseJson.getJSONObject("data")
+                .getJSONObject(when(contentType) {
+                    ContentType.VIDEO -> "videos"
+                    ContentType.CHANNEL -> "channels"
+                })
+
+        return if (resultJson.isNull(id))
+            null
+        else
+            resultJson.getString(id)
+    }
+    
+    private fun startYoutubeActivity() {
+        val intent = Intent(Intent.ACTION_VIEW, intent.data!!)
+        val chooser = Intent.createChooser(intent, resources.getString(R.string.open_youtube))
+        startActivity(chooser)
     }
 }
