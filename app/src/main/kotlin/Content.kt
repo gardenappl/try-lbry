402c6e6f4e24ac9536b6cd37c6ba5effa298a9ce
gardenapple <mailbox@appl.garden>
2023-01-14 19:58:14 +0200

    Add fallbacks for opening YouTube channel

--- app/src/main/kotlin/Content.kt
+++ app/src/main/kotlin/Content.kt
@@ -3,10 +3,14 @@ package garden.appl.trylbry
 import android.net.Uri
 
 data class Content(val type: ContentType, val originalUrl: String, var id: String? = null) {
-    fun toVendorUri(): Uri {
+    /**
+     * Note: NewPipe does not support vnd.youtube for channels
+     */
+    fun toVendorUri(): Uri? {
         return when (type) {
             ContentType.VIDEO -> Uri.parse("vnd.youtube://$id")
-            else -> Uri.parse("vnd.youtube:${Uri.parse(originalUrl).schemeSpecificPart}")
+//            else -> Uri.parse("vnd.youtube:${Uri.parse(originalUrl).schemeSpecificPart}")
+            else -> null
         }
     }
 }
--- app/src/main/kotlin/ContentIntents.kt
+++ app/src/main/kotlin/ContentIntents.kt
@@ -1,9 +1,12 @@
 package garden.appl.trylbry
 
 import android.content.ActivityNotFoundException
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 
 object ContentIntents {
     private fun lbryNameToOdyseeUri(lbryName: String): Uri {
@@ -28,9 +31,114 @@ object ContentIntents {
 
     fun startYoutubeActivity(context: Context, content: Content, lbryNotFoundMessage: Boolean) {
         try {
-            context.startActivity(Intent(Intent.ACTION_VIEW, content.toVendorUri()))
-        } catch (e: ActivityNotFoundException) {
-            context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(content.originalUrl)))
+            content.toVendorUri()?.let { vendorUri ->
+                context.startActivity(Intent(Intent.ACTION_VIEW, vendorUri))
+                return@startYoutubeActivity
+            }
+        } catch (_: ActivityNotFoundException) {}
+
+        val originalIntent = Intent(Intent.ACTION_VIEW, Uri.parse(content.originalUrl))
+        var multipleResolutions = false
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val nonBrowserIntent = Intent(originalIntent)
+            nonBrowserIntent.addFlags(Intent.FLAG_ACTIVITY_REQUIRE_NON_BROWSER)
+
+            val resolutions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                context.packageManager.queryIntentActivities(nonBrowserIntent,
+                    PackageManager.ResolveInfoFlags.of(0))
+            } else {
+                @Suppress("DEPRECATION")
+                context.packageManager.queryIntentActivities(nonBrowserIntent, 0)
+            }
+            if (resolutions.size > 1)
+                multipleResolutions = true
+
+            val resolution = if (resolutions.first().activityInfo.packageName != null
+                && resolutions.first().activityInfo.packageName != context.packageName) {
+
+                resolutions.first()
+            } else {
+                resolutions.removeIf { info ->
+                    info.activityInfo.packageName == context.packageName
+                }
+                if (resolutions.size == 1)
+                    resolutions.first()
+                else
+                    null
+            }
+            if (resolution != null) {
+                nonBrowserIntent.component = ComponentName.createRelative(
+                    resolution.activityInfo.packageName,
+                    resolution.activityInfo.name
+                )
+                context.startActivity(nonBrowserIntent)
+                return
+            }
+        } else {
+            @Suppress("DEPRECATION") val resolutions =
+                context.packageManager.queryIntentActivities(originalIntent, 0)
+            if (resolutions.size > 1)
+                multipleResolutions = true
+
+            val resolution = if (resolutions.first().activityInfo.packageName != null
+                && resolutions.first().activityInfo.packageName != context.packageName) {
+
+                resolutions.first()
+            } else {
+                val foreignResolutions = resolutions.filter { info ->
+                    info.activityInfo.packageName != context.packageName
+                }
+                if (foreignResolutions.size == 1)
+                    foreignResolutions.first()
+                else
+                    null
+            }
+            if (resolution != null) {
+                originalIntent.component = ComponentName(
+                    resolution.activityInfo.packageName,
+                    resolution.activityInfo.name
+                )
+                context.startActivity(originalIntent)
+                return
+            }
+        }
+
+        if (multipleResolutions) {
+            val message = if (lbryNotFoundMessage)
+                R.string.youtube_not_found
+            else
+                R.string.open_youtube
+            val chooser = Intent.createChooser(originalIntent, context.getString(message))
+            context.startActivity(chooser)
+        } else {
+            // Fallback to default browser
+
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://"))
+            val browserResolution = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                context.packageManager.resolveActivity(browserIntent,
+                    PackageManager.ResolveInfoFlags.of(0))
+            } else {
+                @Suppress("DEPRECATION")
+                context.packageManager.resolveActivity(browserIntent, 0)
+            }
+
+            if (browserResolution != null) {
+                originalIntent.component = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    ComponentName.createRelative(
+                        browserResolution.activityInfo.packageName,
+                        browserResolution.activityInfo.name
+                    )
+                } else {
+                    ComponentName(
+                        browserResolution.activityInfo.packageName,
+                        browserResolution.activityInfo.name
+                    )
+                }
+                context.startActivity(originalIntent)
+            } else {
+                throw ActivityNotFoundException("No default browser?")
+            }
         }
     }
 }
